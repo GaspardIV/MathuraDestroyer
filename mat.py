5c28e549c0a799c1e5d94914fd8dd86bb3156b03
gaspardIV <otto.czwarty@gmail.com>
2019-05-06 00:40:40 +0200

    got dicks core curriculum to ex and and conversely

--- mat.py
+++ mat.py
@@ -1,9 +1,12 @@
-# importing required modules 
+# importing required modules
+from collections import Counter
 import PyPDF2
 
 files = ["exams/2015_ANS.pdf", "exams/2016_ANS.pdf", "exams/2017_ANS.pdf", "exams/2018_ANS.pdf"]
 
 dict_year_exercise_to_core_curriculum = dict()
+dict_core_curriculum_to_year_exercise = dict()
+ans_counter = {'A': 0, 'B': 0, 'C': 0, 'D': 0}
 
 for file_name in files:
     with open(file_name, 'rb') as file:
@@ -12,7 +15,23 @@ for file_name in files:
         year = file_name[6:10]
         for i in range(1, 5):  # on pages [1, 2, 3, 4] there are answers for closed questions
             pageObj = pdfReader.getPage(i)
-            print("#] " + year, " p", i, "ex", 0)
-            exercises_on_page = pageObj.extractText().split("Zadanie ")[1::]  # first el is "Strona x z y"
+            exercises_on_page = pageObj.extractText().replace('\n', '').split("Zadanie ")[
+                                1::]  # first el is "Strona x z y"
             for exercise_str in exercises_on_page:
-                print(exercise_str.split('.')[0])
+                ex_num = exercise_str.split('.')[0]
+                l = exercise_str.rfind('(') + 1
+                r = exercise_str.rfind(')')
+                list_of_core_curriculum = exercise_str[l:r:].replace('\n', '').split(", ")
+                dict_year_exercise_to_core_curriculum[year + "_" + ex_num] = list_of_core_curriculum
+                for el in list_of_core_curriculum:
+                    try:
+                        dict_core_curriculum_to_year_exercise[el].append(year + "_" + ex_num)
+                    except:
+                        dict_core_curriculum_to_year_exercise[el] = [year + "_" + ex_num]
+                print(year + "_" + ex_num)
+                right_ans = exercise_str.split("Wersja II ")[1][0]
+                ans_counter[right_ans] += 1
+
+print(dict_year_exercise_to_core_curriculum)
+print(dict_core_curriculum_to_year_exercise)
+print(ans_counter)
